8fb98df6b3bfdb3dadd5f8214a3065007fdfa41e
Anuj Yadav <anuj.y@totalitglobal.com>
2024-04-17 18:09:06 +0530

    template file str changed

--- TA_BugTracker/settings.py
+++ TA_BugTracker/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'TA_BugTracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'BugReport/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'BugReport/static'),
 ]
